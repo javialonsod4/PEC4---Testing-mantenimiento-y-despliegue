960171ecc24abdb5f7f2bd27fc0a2af36b871166
javialonsod4 <166646496+javialonsod4@users.noreply.github.com>
2025-01-08 12:25:23 +0100

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
-from faker import Faker
 from utils import name_surname, minutes_002040
 
 if __name__ == '__main__':
@@ -9,18 +8,18 @@ if __name__ == '__main__':
     # Recreamos el dataframe que hemos ido amoldando.
     # Primer ejercicio.
     df = pd.read_csv('data/dataset.csv', sep=';')
-    
+
     # Segundo ejercicio.
     df = name_surname(df)
     for i in range(len(df)):
         if df.loc[i, 'time'] == '00:00:00':
             df.drop(i, inplace=True)
-    
+
     # TERCER EJERCICIO.
     # Ejecutamos la función 'minutes_002040' a cada uno de los valores
     # de la columna 'time', lo añadimos a una lista y creamos la nueva
     # columna 'time_grouped'.
-    t_grouped = list()
+    t_grouped = []
 
     for i in df['time']:
         t = minutes_002040(i)
@@ -42,11 +41,11 @@ if __name__ == '__main__':
     print(df_grouped)
 
     # Para poder guardar el histograma, debemos crear el nuevo directorio.
-    new_folder = 'img'
+    NEW_FOLDER = 'img'
     try:
-        os.mkdir(new_folder)
+        os.mkdir(NEW_FOLDER)
     except FileExistsError:
-        print('\nFolder "{}" already exists.'.format(new_folder))
+        print(f'\nFolder "{NEW_FOLDER}" already exists.')
 
     # Ahora generamos el histograma y lo guardamos en el nuevo directorio.
     plt.figure()
